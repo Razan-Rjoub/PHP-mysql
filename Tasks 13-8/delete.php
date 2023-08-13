443f6d50a13313f0e7a48ba309dca3c09e994cb7
Razan-Rjoub <ralrjoub6@gmail.com>
2023-08-13 20:37:28 +0300

    CRUD

---
+++ Tasks 13-8/delete.php
@@ -0,0 +1,11 @@
+<?php 
+include 'connection.php';
+$id=$_GET['id'];
+$sql = "DELETE FROM products  WHERE id='$id'";
+if ($conn->query($sql) === TRUE) {
+    echo "Record Deleted successfully";
+    header('Location: table.php');
+} else {
+    echo "Error dDeleting record: " . $conn->error;
+}
+?>
